3b53385622710bbbba790e9c327139b31209ce0a
wkratos22 <michaelwachnicki2001@gmail.com>
2022-03-08 16:06:49 +0100

    esercizio terminato

---
+++ main.js
@@ -0,0 +1,13 @@
+var nome;
+nome = prompt("Come ti chiami?");
+document.write("La tua password è:" + " " + nome);
+
+let cognome = prompt("Come'e il tuo cognome?");
+document.write(cognome);
+
+let colore = prompt("Qual'è il tuo colore preferito?");
+document.write(colore + "21");
+
+
+
+
